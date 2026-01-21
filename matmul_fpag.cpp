266a453633b1ec97ac3dded3987c2a6d447d0b49
warriorli <lishuxuan.0304@gmail.com>
2026-01-21 18:03:28 +0000

    test example

--- matmul_fpag.cpp
+++ matmul_fpag.cpp
@@ -32,6 +32,46 @@ int main() {
         std::cout << "Running on device: "
               << device.get_info<sycl::info::device::name>().c_str()
               << std::endl;
+
+        int *vec_a = new int[kSize];
+        int *vec_b = new int[kSize];
+        int *vec_c = new int[kSize];
+        for(int i = 0; i < kSize; i++) {
+            vec_a[i] = i;
+            vec_b[i] = (kSize- i);
+        }
+
+        {
+            sycl::buffer<int, 1> buf_a(vec_a, sycl::range<1>(kSize));
+            sycl::buffer<int, 1> buf_b(vec_b, sycl::range<1>(kSize));
+            sycl::buffer<int, 1> buf_c(vec_c, sycl::range<1>(kSize));
+
+            q.submit([&](sycl::handler& h) {
+                sycl::accessor accessor_a(buf_a, h, sycl::read_only);
+                sycl::accessor accessor_b(buf_b, h, sycl::read_only);
+                sycl::accessor accessor_c(buf_c, h, sycl::write_only, sycl::no_init);
+
+                h.single_task<VectorAddId>([=]() {
+                    VectorAdd(&accessor_a[0], &accessor_b[0], &accessor_c[0], kSize);
+                });
+            });
+
+            for(int i = 0; i < kSize; i++) {
+                int expected = vec_a[i] + vec_b[i];
+                if(vec_c[i] != expected) {
+                    std::cout << "Mismatch at index " << i << ": "
+                              << "expected " << expected << ", got " << vec_c[i] << std::endl;
+                    passed = false;
+                    break;
+                }
+            }
+
+            std::cout << (passed ? "PASSED" : "FAILED") << std::endl;
+
+            delete[] vec_a;
+            delete[] vec_b;
+            delete[] vec_c;
+        }
     }
     catch(const std::exception& e)
     {

2d42cd17f87f70b330033fdbbda811f07c3c9901
warriorli <lishuxuan.0304@gmail.com>
2026-01-21 11:25:40 +0000

    test device

---
+++ matmul_fpag.cpp
@@ -0,0 +1,10 @@
+#include<sycl/sycl.hpp>
+#include<iostream>
+
+int main(){
+    sycl::queue q;
+    std::cout << "Device::"
+              << q.get_device().get_info<sycl::info::device::name>()
+              << "\n";
+    return 0;
+}
